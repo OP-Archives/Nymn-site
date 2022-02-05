a34447f78a78f74efe9d32d1ea41d1c30abfdf2a
TimIsOverpowered <admin@angelthump.com>
2022-02-05 13:48:23 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Vods from "./vods";
-import VodPlayer from "./vod_player";
+import Vods from "./vods/vods";
 import Navbar from "./navbar";
 import { createTheme, ThemeProvider, responsiveFontSizes } from "@mui/material/styles";
 import { CssBaseline, styled } from "@mui/material";
 import { blue } from "@mui/material/colors";
 import Submission from "./submission";
 import Redirect from "./utils/Redirect";
+import YoutubeVod from "./vods/YoutubeVod";
+import CustomVod from "./vods/CustomVod";
+import NotFound from "./utils/NotFound";
 
 const channel = "nymn",
-  twitchId = "62300805";
+  twitchId = "62300805",
+  VODS_API_BASE = `https://archive.overpowered.tv/${channel.toLowerCase()}`;
 
 export default function App() {
   let darkTheme = createTheme({
@@ -48,12 +51,20 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route
+            path="*"
+            element={
+              <Parent>
+                <NotFound />
+              </Parent>
+            }
+          />
+          <Route
             exact
             path="/"
             element={
               <Parent>
                 <Navbar />
-                <Vods channel={channel} twitchId={twitchId} />
+                <Vods channel={channel} twitchId={twitchId} VODS_API_BASE={VODS_API_BASE} />
               </Parent>
             }
           />
@@ -63,7 +74,7 @@ export default function App() {
             element={
               <Parent>
                 <Navbar />
-                <Vods channel={channel} twitchId={twitchId} />
+                <Vods channel={channel} twitchId={twitchId} VODS_API_BASE={VODS_API_BASE} />
               </Parent>
             }
           />
@@ -72,7 +83,34 @@ export default function App() {
             path="/vods/:vodId"
             element={
               <Parent>
-                <VodPlayer channel={channel} type={"vod"} twitchId={twitchId} />
+                <YoutubeVod channel={channel} twitchId={twitchId} type="vod" VODS_API_BASE={VODS_API_BASE} />
+              </Parent>
+            }
+          />
+          <Route
+            exact
+            path="/live/:vodId"
+            element={
+              <Parent>
+                <YoutubeVod channel={channel} twitchId={twitchId} type="live" VODS_API_BASE={VODS_API_BASE} />
+              </Parent>
+            }
+          />
+          <Route
+            exact
+            path="/youtube/:vodId"
+            element={
+              <Parent>
+                <YoutubeVod channel={channel} twitchId={twitchId} VODS_API_BASE={VODS_API_BASE} />
+              </Parent>
+            }
+          />
+          <Route
+            exact
+            path="/manual/:vodId"
+            element={
+              <Parent>
+                <CustomVod channel={channel} twitchId={twitchId} type="manual" VODS_API_BASE={VODS_API_BASE} />
               </Parent>
             }
           />
--- src/utils/Redirect.js
+++ src/utils/Redirect.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 export default function Redirect(props) {
   useEffect(() => {
     window.location.href = props.redirect;
-  }, []);
+  }, [props.redirect]);
 
   return <></>;
 }
