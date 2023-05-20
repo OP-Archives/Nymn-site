6689c9731deb772e7f9f9241947c5e598ce0e868
TimIsOverpowered <admin@angelthump.com>
2023-05-19 22:15:56 -0500

    fix wrong site for bracket pt 2

--- src/contests/winnerUI.js
+++ src/contests/winnerUI.js
@@ -57,7 +57,7 @@ export default function Winners(props) {
       });
     }
 
-    await fetch(`https://api.nymn.gg/v1/challonge`, {
+    await fetch(`https://contests.nymn.gg/v1/challonge`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
