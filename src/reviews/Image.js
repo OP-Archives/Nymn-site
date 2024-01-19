6fc0395843f621fd78cc792f1226df0cb46ba33c
TimIsOverpowered <admin@angelthump.com>
2024-01-18 19:50:14 -0600

    remove fallback

--- src/reviews/Image.js
+++ src/reviews/Image.js
@@ -1,13 +1,9 @@
 import { Box, Typography, Button } from "@mui/material";
-import { useEffect, useState } from "react";
-
-const IMGUR_CLIENT_ID = process.env.REACT_APP_IMGUR_CLIENT_ID;
-const IMGUR_BASE_API = "https://api.imgur.com/3";
+import { useState } from "react";
 
 export default function Image(props) {
   const submission = props.submission.link;
   const [imageIndex, setImageIndex] = useState(0);
-  const [imgurData, setImgurData] = useState(submission.imgurData);
 
   const nextImage = (_) => {
     const index = imageIndex + 1;
@@ -20,32 +16,6 @@ export default function Image(props) {
     if (!submission.imgurData.images[index]) return;
     setImageIndex(index);
   };
-
-  useEffect(() => {
-    //fallback for old images without imgur data
-    if (submission.imgurData) return;
-
-    const getImageInfo = async () => {
-      const data = await fetch(`${IMGUR_BASE_API}/image/${submission.id}?client_id=${IMGUR_CLIENT_ID}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          if (response.data.error) return null;
-          return response.data;
-        })
-        .catch((e) => {
-          console.error(e);
-          return null;
-        });
-      setImgurData(data);
-    };
-    getImageInfo();
-    return;
-  }, [submission]);
 
   return (
     <Box
@@ -78,7 +48,7 @@ export default function Image(props) {
           <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", pl: 1, pr: 1, pt: 1 }}>
             <Button variant="contained" onClick={prevImage}>{`<`}</Button>
             <Typography sx={{ p: 1 }}>
-              {imageIndex + 1} of {imgurData.images_count}
+              {imageIndex + 1} of {submission.imgurData.images_count}
             </Typography>
             <Button variant="contained" onClick={nextImage}>{`>`}</Button>
           </Box>
@@ -101,7 +71,7 @@ export default function Image(props) {
               width: "100%",
             }}
             alt=""
-            src={submission.imgurData?.link ?? imgurData?.link}
+            src={submission.imgurData?.link ?? submission.link}
           />
         </Box>
       )}
